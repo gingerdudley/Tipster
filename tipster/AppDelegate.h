6bb189ccc1e771b0a2300a2934a574bb1b4e8d77
Ginger Dudley <gingerdudley@fb.com>
2018-06-26 13:24:40 -0700

    Initial Commit

---
+++ tipster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipster
+//
+//  Created by Ginger Dudley on 6/26/18.
+//  Copyright © 2018 Ginger Dudley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipster
+//
+//  Created by Ginger Dudley on 6/26/18.
+//  Copyright © 2018 Ginger Dudley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
